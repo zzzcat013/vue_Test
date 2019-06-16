4e4d0f78cd31957417152fa3b611624ab6e09e01
zzzcat013 <zzzcat013@github.com>
2019-06-16 18:06:34 +0800

    add vue.config.js ok

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	  devServer: {
+		      port: 8085, // 端口号
+		      host: "localhost",
+		      https: false, // https:{type:Boolean}
+		      open: true, //配置自动启动浏览器
+		      // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
+		  //     proxy: {
+		  //           "/api": {
+		  //                   target: "<url>",
+		  //                           ws: true,
+		  //                                   changeOrigin: true
+		  //                                         },
+		  //                                               "/foo": {
+		  //                                                       target: "<other_url>"
+		  //                                                             }
+		  //                                                                 } // 配置多个代理
+		  //                                                                   }
+		  //                                                                   };
